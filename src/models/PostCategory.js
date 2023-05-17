9b54d48015dc95e06ebcd246589373fc806e82f2
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-05-16 21:05:34 -0300

    Req 11

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -5,9 +5,36 @@
  */
 const PostCategorySchema = (sequelize, DataTypes) => {
   const PostCategoryTable = sequelize.define("PostCategory", {
-    post_id: DataTypes.NUMBER,
-    category_id: DataTypes.NUMBER,
+    postId: {
+      allowNull: false,
+      type: DataTypes.NUMBER,
+      foreignKey: true,
+    },
+    categoryId: {
+      allowNull: false,
+      type: DataTypes.NUMBER,
+      foreignKey: true,
+    }
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  },
+)
+PostCategoryTable.associate = (models) => {
+  models.Category.belongsToMany(models.BlogPost, {
+    as: 'blogposts',
+    through: PostCategoryTable,
+    foreignKey: 'postId',
+    otherKey: 'categoryId'
+  });
+  models.BlogPost.belongsToMany(models.Category, {
+    as: 'Category',
+    through: PostCategoryTable,
+    foreignKey: 'categoryId',
+    otherKey: 'postId'
   })
+};
   return PostCategoryTable;
 }
 
